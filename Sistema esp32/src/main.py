690510fc736392b931b40a53be862f6819e634b5
JohnWilliam24dev <seu@email.com>
2025-02-25 15:14:57 -0300

    generic

---
+++ Sistema esp32/src/main.py
@@ -0,0 +1,41 @@
+from machine import Pin
+from time import sleep 
+import network
+
+wlan = network.WLAN(network.WLAN.IF_STA)
+wlan.active(True)
+wlan.scan()
+wlan.isconnected()
+wlan.connect('ssid', 'key') #ssid e senha
+led= Pin(21,Pin.OUT)
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Inicialize as credenciais com o caminho para o seu arquivo JSON de chave de serviço
+cred = credentials.Certificate("caminho do certificado")
+firebase_admin.initialize_app(cred)
+
+
+db = firestore.client()
+
+count_test
+doc_ref = db.collection("token")
+doc = doc_ref.get()
+
+if doc.exists:
+    print("Dados do documento:", doc.to_dict())
+else:
+    print("Documento não encontrado!")
+
+# Exemplo 2: Recuperar todos os documentos de uma coleção
+usuarios_ref = db.collection("usuarios")
+docs = usuarios_ref.stream()
+
+print("Documentos na coleção 'usuarios':")
+for documento in docs:
+    print(f"{documento.id} => {documento.to_dict()}")
+
+while True:
+    led.value(1)
+
+    
